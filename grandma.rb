020e1a9d1675c9eed4b9c750266703256c78d1a7
Paige Wiener <snowplushie@yahoo.com>
2019-02-27 20:54:41 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,5 +1,5 @@
 def speak_to_grandma(phrase)
-  if phrase != phrase.uppercase
+  if phrase != phrase.upcase()
     return "I LOVE YOU TOO PUMPKIN"
   elsif phrase = "Hi Nana, how are you?"
     return "HUH?! SPEAK UP, SONNY!"
