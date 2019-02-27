66ff35cc3db21aed26401188a0f6eddbddce8ec3
Paige Wiener <snowplushie@yahoo.com>
2019-02-27 20:49:29 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,3 +1,7 @@
+def speak_to_grandma(phrase)
+  if phrase = "I LOVE YOU GRANDMA"
+    return "I LOVE YOU TOO PUMPKIN"
+
 # Write a speak_to_grandma method.
 
 # Whatever you say to grandma, she should respond with
