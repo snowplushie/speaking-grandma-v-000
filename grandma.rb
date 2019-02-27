af1ba6db2daf41ea6c74c628f1ecda250fdc7c68
Paige Wiener <snowplushie@yahoo.com>
2019-02-27 21:01:39 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,5 +1,5 @@
 def speak_to_grandma(phrase)
-  if phrase != phrase.upcase()
+  if phrase != phrase.upcase
     return "HUH?! SPEAK UP, SONNY!"
   elsif phrase.upcase && phrase != "I LOVE YOU GRANDMA"
     return "NO, NOT SINCE 1938!"
